6b46a422881dffa950e3caeea50d3facda1f33d9
hyojin-park24 <gygy332@naver.com>
2021-06-23 18:11:55 +0900

    Device Json DB연결 커밋

---
+++ MRPApp/Model/StockStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRPApp.Model
+{
+    public class StockStore : Store
+    {
+        public int StockQuantity { get; set; }
+    }
+}
