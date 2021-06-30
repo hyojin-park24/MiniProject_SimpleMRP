03939752d717f6cdc321bf8af7f3f4b57f1bca4e
hyojin-park24 <qkrgywls332@gmail.com>
2021-06-30 21:10:07 +0900

    0630 ReportView.xaml.cs 추가해야함

--- MRPApp/MainWindow.xaml.cs
+++ MRPApp/MainWindow.xaml.cs
@@ -16,10 +16,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MRPApp.View;
 using MRPApp.View.Account;
+using MRPApp.View.Store;
 using MRPApp.View.Setting;
 using MRPApp.View.Schedule;
 using System.Configuration;
 using MRPApp.View.Process;
+using MRPApp.View.Report;
 
 namespace MRPApp
 {
@@ -33,14 +35,12 @@ namespace MRPApp
             InitializeComponent();
         }
 
-        //private void MetroWindow_ContentRendered(object sender, EventArgs e)
-        //{
-        //    ShowLoginView();
-        //}
+        private void MetroWindow_ContentRendered(object sender, EventArgs e)
+        {
+        }
 
         private void MetroWindow_Activated(object sender, EventArgs e)
-       {
-
+        {
             Commons.PLANTCODE = ConfigurationManager.AppSettings.Get("PlantCode");
             Commons.FACILITYID = ConfigurationManager.AppSettings.Get("FacilityID");
             try
@@ -52,51 +52,15 @@ namespace MRPApp
             {
                 Commons.LOGGER.Error($"예외발생 : {ex}");
             }
-
-
         }
 
-        //private async void BtnLogOut_Click(object sender, RoutedEventArgs e)
-        //{
-        //    // TODO : 모든 화면을 해제하고 첫화면으로 돌려놔야 함.
-
-        //    var result = await this.ShowMessageAsync("로그아웃", "로그아웃하시겠습니까?",
-        //        MessageDialogStyle.AffirmativeAndNegative, null);
-
-        //    if (result == MessageDialogResult.Affirmative)
-        //    {
-        //        Commons.LOGINED_USER = null; // 로그인했던 사용자 정보를 삭제
-        //        ShowLoginView();
-        //    }
-        //}
-
-        //private void ShowLoginView()
-        //{
-        //    LoginView view = new LoginView();
-        //    view.Owner = this;
-        //    view.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-        //    view.ShowDialog();
-        //}
         private async void BtnExit_Click(object sender, RoutedEventArgs e)
         {
-            var result = await this.ShowMessageAsync("종료","프로그램을 종료하시겠습니까?",
-                                    MessageDialogStyle.AffirmativeAndNegative, null);
+            var result = await this.ShowMessageAsync("종료", "프로그램을 종료하시겠습니까?",
+                MessageDialogStyle.AffirmativeAndNegative, null);
 
             if (result == MessageDialogResult.Affirmative)
                 Application.Current.Shutdown();
-        }
-
-        private async void BtnAccount_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                ActiveControl.Content = new MyAccount();
-            }
-            catch (Exception ex)
-            {
-                Commons.LOGGER.Error($"예외발생 BtnAccount_Click : {ex}");
-                await this.ShowMessageAsync("예외", $"예외발생 : {ex}");
-            }
         }
 
         private void BtnSetting_Click(object sender, RoutedEventArgs e)
@@ -112,7 +76,7 @@ namespace MRPApp
             }
         }
 
-        private void Btnshcedul_Click(object sender, RoutedEventArgs e)
+        private void BtnSchedule_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -120,7 +84,7 @@ namespace MRPApp
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 BtnSetting_Click : {ex}");
+                Commons.LOGGER.Error($"예외발생 BtnSchedule_Click : {ex}");
                 this.ShowMessageAsync("예외", $"예외발생 : {ex}");
             }
         }
@@ -133,7 +97,20 @@ namespace MRPApp
             }
             catch (Exception ex)
             {
-                Commons.LOGGER.Error($"예외발생 BtnSetting_Click : {ex}");
+                Commons.LOGGER.Error($"예외발생 BtnMonitoring_Click : {ex}");
+                this.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
+        }
+
+        private void BtnReport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ActiveControl.Content = new ReportView();
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnReport_Click : {ex}");
                 this.ShowMessageAsync("예외", $"예외발생 : {ex}");
             }
         }
